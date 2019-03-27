0247eacae108816270e0c122948a889a06fd324d
DiamondMohanty <memohanty.diamond@gmail.com>
2019-03-28 01:51:46 +0530

    SQL Insert Script

---
+++ SQL/module.sql
@@ -0,0 +1,6 @@
+insert into module_master (path, location, moduleName, description) values ('moduleA', './assets/modulea.umd.js', 'ModuleaModule', 'clarity form');
+insert into module_master (path, location, moduleName, description) values ('moduleB', './assets/moduleb.umd.js', 'ModulebModule', 'clarity tab control with table and card - custom elements');
+insert into module_master (path, location, moduleName, description) values ('moduleC', './assets/modulec.umd.js', 'ModulecModule', 'clarity modal - custom elements');
+insert into module_master (path, location, moduleName, description) values ('moduled', './assets/moduled.umd.js', 'ModuledModule', 'default subnavigation with 3 components and some clarity stuff');
+
+COMMIT;
